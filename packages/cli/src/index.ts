1fb7d593094eec954ff60fd2f0c54e16af3a0021
Kumac13 <foohilack@gmail.com>
2025-10-13 21:15:58 +0900

    chore: show comment help without stack traces for -h

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -6,6 +6,26 @@ if (argv[0] === '--') {
   argv.shift();
 }
 
+if (
+  argv[0] === 'memo' &&
+  argv[1] === 'comment' &&
+  argv.slice(2).some((arg) => arg === '-h' || arg === '--help')
+) {
+  console.log(
+    'Usage:\n' +
+      '  mgtd memo comment <memoId> [--json]\n' +
+      '  mgtd memo comment add <memoId> --body "comment"\n' +
+      '  mgtd memo comment edit <memoId> <commentId> --body "new body"\n' +
+      '  mgtd memo comment delete <memoId> <commentId> --yes'
+  );
+  console.log('\nSubcommands:');
+  console.log('  add    add a new comment (--body / --body-file)');
+  console.log('  edit   update an existing comment (--body / --body-file)');
+  console.log('  delete remove a comment (--yes to skip confirmation)');
+  console.log('\nTo list comments only, run `mgtd memo comment <memoId>`');
+  process.exit(0);
+}
+
 const normalizedArgv = argv.map((arg) => (arg === '-h' ? '--help' : arg));
 
 run(normalizedArgv, import.meta.url)
