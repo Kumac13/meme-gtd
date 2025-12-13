6b2c6a61c3fa611dffd6ee3e46d92e4b4b4c6e77
Kumac13 <foohilack@gmail.com>
2025-12-13 11:24:11 +0900

    chore(web): sync API client with backend and fix untracked files
    - Sync TasksService with latest API changes (projectIds, linkIds)
    - Add previously untracked generated service files (ActivityLogService, AttachmentsService, etc.)

---
+++ packages/web/src/api/index.ts
@@ -0,0 +1,18 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export { ActivityLogService } from './services/ActivityLogService';
+export { ArticlesService } from './services/ArticlesService';
+export { AttachmentsService } from './services/AttachmentsService';
+export { CommentsService } from './services/CommentsService';
+export { LabelsService } from './services/LabelsService';
+export { LinksService } from './services/LinksService';
+export { MemosService } from './services/MemosService';
+export { ProjectsService } from './services/ProjectsService';
+export { TasksService } from './services/TasksService';
